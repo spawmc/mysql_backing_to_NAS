857337c57a0a4551213c85134b30182b1ed976f6
spawmc <spawnmcsqrt@gmail.com>
2021-05-02 02:17:43 -0500

    added script

---
+++ backup_scheme.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+NAME="Respaldo"
+DB_NAME="Entidad_sanitaria"
+DB_USER=spawn
+DB_MY_CNF=/usr/bin/databases/.my.cnf
+DATE=$(date +"%d%m%y%H%M")
+FILE_NAME=${NAME}${DB_NAME}${DATE}.sql
+SMB_IP=172.17.0.4
+SMB_FOLDER="sambashare"
+SMB_USER="spawnmc"
+SMB_PASSWD="joshuan9819"
+SHARE_MOUNT_DIR=/mnt/sambashare
+MAIL_MSG="El backup se ha generado con éxito y se encuentra actualmente en el recurso compartido"
+MAIL_DEST="spawnmcsqrt@gmail.com"
+
+generete_backup(){
+    echo "Generando backup..."
+    mysqldump --defaults-file=$DB_MY_CNF -u $DB_USER $DB_NAME --no-data --skip-comments --routines > $FILE_NAME
+}
+
+
+make_dir(){
+    if [[ -d $SHARE_MOUNT_DIR ]] ; then
+        echo "Directorio para recursos compartidos existe."
+    else
+        sudo mkdir $SHARE_MOUNT_DIR
+        echo "Directorio de recursos compartido creado."
+    fi
+}
+
+
+mount_shared_resource(){
+    if ! df | grep -i $SMB_IP &>/dev/null ; then
+        echo "Montando recurso compartido..."
+        sudo mount -t cifs -o username=${SMB_USER},password=${SMB_PASSWD} //${SMB_IP}/${SMB_FOLDER} ${SHARE_MOUNT_DIR}
+    else
+        echo "El recurso compartido ya se encuentra montado"
+    fi
+}
+
+copy_to_shared_resource(){
+    echo "Copiando backup a recurso compartido..."
+    if cp $FILE_NAME $SHARE_MOUNT_DIR -f ; then
+        rm  $FILE_NAME
+    fi
+}
+
+
+send_mail(){
+    if ping -c 2 www.google.com &>/dev/null ; then
+        echo $MAIL_MSG | mail -s "Backup generado" $MAIL_DEST
+        echo "Correo electronico enviado." 
+    else
+        echo "Sin acceso a internet"
+        echo "Correo electronico no enviado"
+    fi
+}
+
